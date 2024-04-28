52b1d290c084f73b3710080f3bd65e5a2829f671
user <dupa@dupa>
2024-04-28 19:27:54 +0200

    własny projekt

---
+++ base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+# Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets learn Python'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'},
+]
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)
+
+def room(request, pk):
+    return render(request, 'base/room.html')
